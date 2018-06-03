d9d189a4410760ff9c2aae5309a995d8d7ee4401
john andrey <andrey_201015@hotmail.com>
2018-06-03 12:01:21 -0500

    Guardando sessiones en la data base con mongodb

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// mongoose.connect('mongodb://192.168.99.100:32768/prueba');
+// Se conecto a un container de docker
+const connection = mongoose.createConnection('mongodb://192.168.99.100:32768/auth');
 
 const app = express();
 const port = 3000;
@@ -8,8 +14,12 @@ const port = 3000;
 // Esto es un middleware -> La función se ejecuta cada vez que la aplicación recibe una solicitud.
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true
+  // resave: true, investigar mas -> https://www.npmjs.com/package/express-session
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: connection,
+  })
 }));
 
 app.get('/', (req, res) => {
